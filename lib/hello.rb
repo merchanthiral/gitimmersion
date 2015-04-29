ca4d49e190dd9c0900da6c38f6f3f899ed245813
merchanthiral <hmercha1@uncc.edu>
2015-04-28 22:47:16 -0300

    Made interactive

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,4 @@
-# Default is World
-# Author: Hiral Merchant - hmercha1@uncc.edu
-name = ARGV.first || "World"
+puts "What's your name"
+my_name = gets.strip
 
-puts "Hello, #{name}!"
-
+puts "Hello, #{my_name}!"
